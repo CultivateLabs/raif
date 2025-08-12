8722e3c802320d4396dba09da8c6bd109067b77b
Ben Roesch <ben@cultivatelabs.com>
2025-08-12 14:18:37 -0400

    Move llm judges so they don’t get autoloaded

--- lib/raif/evals.rb
+++ lib/raif/evals.rb
@@ -4,6 +4,10 @@ require "raif/evals/expectation_result"
 require "raif/evals/eval"
 require "raif/evals/eval_set"
 require "raif/evals/run"
+require "raif/evals/llm_judge"
+require "raif/evals/llm_judges/binary"
+require "raif/evals/llm_judges/comparative"
+require "raif/evals/llm_judges/scored"
 require "raif/evals/scoring_rubric"
 
 module Raif
