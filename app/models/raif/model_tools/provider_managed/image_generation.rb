9cb331af83a52beebb940d9185806e0f2ff1ab57
Ben Roesch <ben@cultivatelabs.com>
2025-10-23 16:00:28 -0400

    Add model tools display to admin pages

--- app/models/raif/model_tools/provider_managed/image_generation.rb
+++ app/models/raif/model_tools/provider_managed/image_generation.rb
@@ -2,4 +2,8 @@
 
 class Raif::ModelTools::ProviderManaged::ImageGeneration < Raif::ModelTools::ProviderManaged::Base
 
+  tool_description do
+    "Utilizes the model provider's built-in image generation capabilities."
+  end
+
 end
