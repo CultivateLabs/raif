904981c07cf3361b4e9df65c47653608613ee637
Ben Roesch <ben@cultivatelabs.com>
2025-11-28 09:53:06 -0600

    Preserve older rails compatibility

--- db/migrate/20251124185033_add_provider_tool_call_id_to_raif_model_tool_invocations.rb
+++ db/migrate/20251124185033_add_provider_tool_call_id_to_raif_model_tool_invocations.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddProviderToolCallIdToRaifModelToolInvocations < ActiveRecord::Migration[8.0]
+class AddProviderToolCallIdToRaifModelToolInvocations < ActiveRecord::Migration[7.2]
   def change
     add_column :raif_model_tool_invocations, :provider_tool_call_id, :string
   end
