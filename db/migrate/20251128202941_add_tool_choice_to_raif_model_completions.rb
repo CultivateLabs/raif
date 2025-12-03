67cb5f3aedeeef6940f2eac91b09c4e6a819f0ce
Ben Roesch <ben@cultivatelabs.com>
2025-12-03 10:51:16 -0500

    Preserve older rails compatibility

--- db/migrate/20251128202941_add_tool_choice_to_raif_model_completions.rb
+++ db/migrate/20251128202941_add_tool_choice_to_raif_model_completions.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddToolChoiceToRaifModelCompletions < ActiveRecord::Migration[8.1]
+class AddToolChoiceToRaifModelCompletions < ActiveRecord::Migration[7.2]
   def change
     add_column :raif_model_completions, :tool_choice, :string
   end
