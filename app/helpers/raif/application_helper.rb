fff6ab7686e1b1d62100c6768e3232ed1e2a185a
Ben Roesch <ben@cultivatelabs.com>
2026-03-09 12:00:14 -0400

    Pretty print json responses in prompt studio

--- app/helpers/raif/application_helper.rb
+++ app/helpers/raif/application_helper.rb
@@ -3,5 +3,15 @@
 module Raif
   module ApplicationHelper
     include Pagy::Frontend
+
+    def format_task_response(task)
+      if task.response_format_json? && task.raw_response.present?
+        JSON.pretty_generate(JSON.parse(task.raw_response))
+      else
+        task.raw_response
+      end
+    rescue JSON::ParserError
+      task.raw_response
+    end
   end
 end
