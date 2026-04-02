633a3ca42745e74a1918c5139877f8b74c9ea84b
Ben Roesch <ben@cultivatelabs.com>
2026-04-02 14:58:22 -0400

    Add admin page for listing llm registry

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,6 @@ pin "raif/admin/judge_config_controller", to: "raif/admin/judge_config_controlle
 pin "raif/admin/select_all_checkboxes_controller", to: "raif/admin/select_all_checkboxes_controller.js"
 pin "raif/admin/cost_estimate_controller", to: "raif/admin/cost_estimate_controller.js"
 pin "raif/admin/tom_select_controller", to: "raif/admin/tom_select_controller.js"
+pin "raif/admin/table_search_controller", to: "raif/admin/table_search_controller.js"
+pin "raif/admin/sortable_table_controller", to: "raif/admin/sortable_table_controller.js"
 pin "raif/stream_actions/raif_scroll_to_bottom", to: "raif/stream_actions/raif_scroll_to_bottom.js"
