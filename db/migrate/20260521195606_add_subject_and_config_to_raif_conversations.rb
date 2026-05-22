655bb1e947687928e0d8f5ba5013587b1a9f5918
Ben Roesch <ben@cultivatelabs.com>
2026-05-21 21:32:03 -0400

    Keep older version rails compatibility

--- db/migrate/20260521195606_add_subject_and_config_to_raif_conversations.rb
+++ db/migrate/20260521195606_add_subject_and_config_to_raif_conversations.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddSubjectAndConfigToRaifConversations < ActiveRecord::Migration[8.1]
+class AddSubjectAndConfigToRaifConversations < ActiveRecord::Migration[7.1]
   def change
     add_reference :raif_conversations, :subject, polymorphic: true, index: true
     add_column :raif_conversations, :config, :jsonb, null: false, default: {}
